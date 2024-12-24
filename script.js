36f093577d092c50a85ea5ecfaa723d87d160029
wTaehee <taehee.whang@gmail.com>
2024-12-23 23:50:30 -0500

    entry

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const imageContainers = document.querySelectorAll('.image-container');
 const centerImage = document.getElementById('center-image');
+const centerName = document.getElementById('center-name');
 const centerTitle = document.getElementById('center-title');
+const centerYear = document.getElementById('center-year');
 const centerBody = document.getElementById('center-body');
 const centerDisplay = document.querySelector('.center-display');
 
 let isImageClicked = false;
 
 imageContainers.forEach(container => {
+    const name = container.getAttribute('data-name');
     const title = container.getAttribute('data-title');
+    const year = container.getAttribute('data-year');
     const body = container.getAttribute('data-body');
     const imageSrc = container.querySelector('img').src;
 
@@ -15,7 +19,9 @@ imageContainers.forEach(container => {
     container.addEventListener('mouseenter', function () {
         if (!isImageClicked) {
             centerImage.src = imageSrc;
+            centerName.textContent = name;
             centerTitle.textContent = title;
+            centerYear.textContent = year;
             centerBody.textContent = body;
             centerDisplay.style.display = 'block';
         }
@@ -28,7 +34,9 @@ imageContainers.forEach(container => {
             isImageClicked = false;
         } else {
             centerImage.src = imageSrc;
+            centerName.textContent = name;
             centerTitle.textContent = title;
+            centerYear.textContent = year;
             centerBody.textContent = body;
             centerDisplay.style.display = 'block';
             isImageClicked = true;
